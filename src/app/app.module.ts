54c035355006e2415ab3252fae487a6002f7fab5
Iago Felipe Morais da Silva <iagosilva@mobitbrasil.com.br>
2021-11-19 15:36:20 -0300

    [INICIANDO PROJETO FRONT] - Concluindo esqueleto do front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TemplateModule} from "./template/template.module";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        TemplateModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
