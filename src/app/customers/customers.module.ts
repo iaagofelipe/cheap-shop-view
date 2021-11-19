f2d853d0ee50e0e509a3815d68375ca64593e4fb
Iago Felipe Morais da Silva <iagosilva@mobitbrasil.com.br>
2021-11-19 16:44:54 -0300

    [Navegação] - Navegando através de router links

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {CustomersRoutingModule} from './customers-routing.module';
+import {CustomersFormComponent} from './customers-form/customers-form.component';
+
+@NgModule({
+  declarations: [CustomersFormComponent],
+  imports: [
+    CommonModule,
+    CustomersRoutingModule
+  ], exports: [
+    CustomersFormComponent
+  ]
+})
+export class CustomersModule {
+}
